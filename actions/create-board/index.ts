31e05da0fead0a9ffbe8021ce58215c1545c61da
Kalepu Satya Sai Teja <mail@kssaiteja.me>
2023-11-29 12:25:15 +0530

    board popover started 4.48.59

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -1,2 +1,40 @@
-// ** video stopped at 4.01.25
+"use server"
 
+import { auth } from "@clerk/nextjs";
+import { InputType, ReturnType } from "./types";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { CreateBoard } from "./schema";
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+
+  const { title } = data;
+
+  let board;
+
+  try {
+    board = await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (error) {
+    return {
+      error: "Unable to create board",
+    };
+  }
+
+  revalidatePath(`/board/${board.id}`);
+
+  return { data: board };
+};
+
+export const createBoard = createSafeAction(CreateBoard, handler);
